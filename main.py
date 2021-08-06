d9526789b32b57ee48bb286668d02db12d6739bd
TimothyFothergill <timothy.fothergill@digital.hmrc.gov.uk>
2021-08-06 23:48:55 +0100

    CVG-1013 Add basic Flask support.

--- main.py
+++ main.py
@@ -2,52 +2,62 @@ import datetime
 import http.client
 import json
 import os
+from flask import Flask, render_template
 
 from dotenv import load_dotenv
 
-load_dotenv()
+app = Flask(__name__)
 
-# set .env variables to match. api key obtained from: 
-# https://dashboard.api-football.com
+@app.route("/")
+def index():
+    load_dotenv()
 
-key = os.getenv('MY_API_KEY')
-season = os.getenv('SEASON')
-team = os.getenv('TEAM')
-get_next = os.getenv('GET_NEXT')
+    # set .env variables to match. api key obtained from: 
+    # https://dashboard.api-football.com
 
-conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-headers = {
-    'x-rapidapi-host': "v3.football.api-sports.io",
-    'x-rapidapi-key': key
-    }
-q = "season=" + str(season) + "&team=" + str(team) + "&next=" + str(get_next)
-conn.request("GET", "/fixtures?" + str(q), headers=headers)
+    key = os.getenv('MY_API_KEY')
+    season = os.getenv('SEASON')
+    team = os.getenv('TEAM')
+    get_next = os.getenv('GET_NEXT')
+    venue = os.getenv('VENUE_ID')
 
-res = conn.getresponse()
-data = res.read()
-j = json.loads(data)
+    conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+    headers = {
+        'x-rapidapi-host': "v3.football.api-sports.io",
+        'x-rapidapi-key': key
+        }
+    q = "season=" + str(season) + "&team=" + str(team) + "&next=" + str(get_next)
+    conn.request("GET", "/fixtures?" + str(q), headers=headers)
 
-for i in j["response"]:
+    res = conn.getresponse()
+    data = res.read()
+    j = json.loads(data)
 
-    match_date = datetime.datetime.strptime(
-        str(i["fixture"]["date"]), 
-        "%Y-%m-%d" + "T" + "%H:%M:%S" + "+00:00"
-        ).date()
-    current_date = datetime.date.today()
-    match_date = match_date.strftime("%d %b %Y")
-    current_date = current_date.strftime("%d %b %Y")
+    for i in j["response"]:
 
-    print("Today's Date is: " + str(current_date))
-    print("The next match is on: " + str(match_date))
+        match_date = datetime.datetime.strptime(
+            str(i["fixture"]["date"]), 
+            "%Y-%m-%d" + "T" + "%H:%M:%S" + "+00:00"
+            ).date()
+        current_date = datetime.date.today()
+        match_date = match_date.strftime("%d %b %Y")
+        current_date = current_date.strftime("%d %b %Y")
 
-    if(i["fixture"]["venue"]["id"] == 510):
-        if(match_date == currentDate):
-            print("UH-OH! The football is going to be playing at home. Do " + 
-            "not move the car!")
+        print("Today's Date is: " + str(current_date))
+        print("The next match is on: " + str(match_date))
+
+        if(i["fixture"]["venue"]["id"] == venue):
+            if(match_date == currentDate):
+                return render_template("index.html", RESULT= """UH-OH! The 
+                football is going to be playing at home. Do not move the 
+                car!""")
+            else:
+                return render_template("index.html", RESULT="""WARNING: The  
+                next Rovers match will be at home on:""" + str(match_date) 
+                + """. Until then, you can move the car.""")
         else:
-            print("WARNING: The next Rovers match will be at home on: " + 
-                str(match_date) + 
-                ". Until then, you can move the car")
-    else:
-        print("The football is playing away for their next game." + 
-        " You are free to move the car.")
+            return render_template("index.html", RESULT="""The football is 
+            playing away for their next game. You are free to move the car.""")
+
+if __name__ == "__main__":
+    app.run("localhost")
